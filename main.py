ba30c98e9ddba95b3a3b0ef4e5aa4ead5b7633e6
Islamisk <islam.ybt@gmail.com>
2022-12-30 14:40:46 +0300

    hw7

--- main.py
+++ main.py
@@ -1,27 +1,12 @@
-import colorama
-import math,datetime
-
-# MRO
-
 class Human:
     def __init__(self,name,age):
         self.name=name
         self.age=age
-    # def __str__(self):
-    #     return f'меня зовут {self.name} и мне {self.age} лет'
-
-hum=Human('Бекболот',21)
-hum.age=45
-print(hum)
 
 class Human2(Human):
     def __init__(self,name,age,nasa=False):
         super().__init__(name,age)
         self.nasa=nasa
-    # def _n(self):
-    #     self.nasa=True
-    # def t(self):
-    #     self.age *= 2
     def __orientation(self):
         print(f'{self.name} скрывает свою ориентацию')
     def year(self):
@@ -57,5 +42,7 @@ class Human2(Human):
             Human2.car(self)
         elif a == 5:
             Human2.__orientation(self)
+        elif a == 6:
+            quit()
 w = Human2('Mirdjalalydin', 23)
 w.wh()
